13a60b5b847db5a0cbe20de0b940570b5b1cc410
Lucas David Erkana <lderkana21x@gmail.com>
2023-07-10 19:13:43 +0200

    Configure Devise and associations in User model
    •	Configured Devise modules for user authentication, including database authentication, registration, password recovery, and session management.
    •	Added a has_many association to categories in the User model using the foreign_key 'author_id'.
    •	Added a has_many association to entities in the User model using the foreign_key 'author_id'.
    •	Added presence and length validations for the name field in the User model, ensuring it is required and within a maximum length of 50 characters.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ApplicationRecord
-    belongs_to :author, class_name: 'User', foreign_key: :author_id
-    has_and_belongs_to_many :entities
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+  has_and_belongs_to_many :entities
 
-    validates :name, presence: true
-    validates :icon, presence: true
+  validates :name, presence: true
+  validates :icon, presence: true
 end
