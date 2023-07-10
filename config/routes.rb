7a46b20d115c99c291bcab97abc86566bbfd9ccb
Lucas David Erkana <lderkana21x@gmail.com>
2023-07-10 17:57:25 +0200

    Configure routes
    - Configure routes for user authentication using Devise.
    - Define routes for the splash page and set it as the root for unauthenticated users.
    - Set the root path for authenticated users to the categories index.
    - Define nested routes for users, categories, and entities with specific actions allowed.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,28 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :categories
-  resources :entities
-  resources :users
+  # resources :categories
+  # resources :entities
+  # resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get '/splash', to: 'splashes#index'
+
+
+  devise_scope :user do
+    authenticated :user do
+      root 'categories#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root "splashes#index", as: :unauthenticated_root
+    end
+  end
+
+  resources :users, only: [:index, :show, :new] do
+    resources :categories, only: [:new, :create, :show, :index, :destroy]
+    resources :entities, only: [:new, :create, :index, :show, :destroy]
+  end
 end
