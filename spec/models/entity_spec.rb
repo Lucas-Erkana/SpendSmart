5b6421e2909a226f580f4a6c188881f707add229
Lucas David Erkana <lderkana21x@gmail.com>
2023-07-11 12:05:53 +0200

    Model tests
    -rails generate rspec:install
    -rails db:migrate RAILS_ENV=test
    -category_spec
    -entity_spec.rb
    -user_spec.rb

--- spec/models/entity_spec.rb
+++ spec/models/entity_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Entity, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.create(name: 'John Doe') }
+  subject do
+    Entity.new(name: 'Food', amount: 10, author: user)
+  end
+
+  it 'should not be valid with name value is nil' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'should not be valid if amount is less than 0' do
+    subject.amount = -5
+    expect(subject).to_not be_valid
+  end
+
+  it 'Should be valid' do
+    expect(subject).to be_valid
+  end
 end
