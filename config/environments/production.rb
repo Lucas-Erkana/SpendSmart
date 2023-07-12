080cc2775ec47d8ffd4b5946c5dcc0a736462ddc
Lucas David Erkana <lderkana21x@gmail.com>
2023-07-12 05:37:56 +0200

    development product

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "SpendSmart_production"
+  # config.active_job.queue_name_prefix = "SmartSaver_production"
 
   config.action_mailer.perform_caching = false
 
